be06188197bef44671e61976ee96b50f3b00c860
ABHIBARNWAL <abhishekranjan74920@gmail.com>
2023-12-10 19:21:31 +0530

    router and controller commit

--- src/app.js
+++ src/app.js
@@ -8,18 +8,23 @@ app.use(cors({
     origin:process.env.CORS_ORIGIN,
     credentials:true
 }))
-
 app.use(express.json({
     limit:"16kb"
 }))
-
 app.use(urlencoded({
     extended:true,
     limit:"16kb"
 }))
-
 app.use(express.static("public"))
-
 app.use(cookieParser())
 
+
+//Routes import
+import router from "./routes/user.routes.js"
+
+
+
+//Router declaration
+app.use("/api/v1/users",router)
+// http:localhost:8000/api/v1/users/register
 export {app};
